6420f524646f0740ecea3dfe16bbbfe9aea89633
eiriso16 <eiriksolaas@hotmail.com>
2018-09-25 11:03:19 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.use(bodyParser.json());
 app.listen(app.get('port'), function(){
 console.log('server running', app.get('port'));
 });
+
+const users = [];
+
+app.post("/api/user",function(req,res){
+    //res.send("WOHO").end();
+    
+    let user = req.body;
+    
+    user.id = users.length +1;
+    users.push(user);
+    
+    res.json(user).end();
+})
