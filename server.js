ff8f08a12cc9f51056aa79049a951b8659e47bab
vibsterb <42640854+vibsterb@users.noreply.github.com>
2018-09-26 11:42:12 +0200

    Update server.js
    Lagt til user login og en litt utvidet versjon av create user

--- server.js
+++ server.js
@@ -12,13 +12,43 @@ console.log('server running', app.get('port'));
 
 const users = [];
 
+//create user
 app.post("/api/user",function(req,res){
-    //res.send("WOHO").end();
-    
-    let user = req.body;
-    
-    user.id = users.length +1;
-    users.push(user);
-    
-    res.json(user).end();
+	
+	let user = req.body;
+	let isUser = false;
+	
+	for(let i in users){
+		if(user.email === users[i].email){
+			isUser = true;
+		}
+	}
+	
+	if(!isUser){
+		user.id = users.length + 1;
+		users.push(user);
+		res.json(user).end();
+	}
+	else
+		res.json("User already exists, try another email address").end();
+})
+
+//user login
+app.post("/api/login",function(req,res){
+	
+	let user = req.body;
+	let accept = false;
+	let name = "";
+	
+	for(let i in users){
+		if(user.email === users[i].email && user.password === users[i].password){
+			accept = true;
+			name = users[i].name;
+		}
+	}
+	if(accept){
+		res.json("Hello " + name).end();
+	}
+	else 
+		res.json("Wrong email or password").end();
 })
